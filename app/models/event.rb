bbebd4e0ff9368d905cc41a2f52597ffe704bddc
Boston <bostoncessary@gmail.com>
2024-02-29 03:35:40 -0700

    Add event controller actions and views

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,14 @@ class Event < ApplicationRecord
     validates :title, presence: true
     validates :location, presence: true
     validates :event_date_time, presence: true
+    validate :event_date_time_cannot_be_in_past
 
+
+    
     private
 
     def event_date_time_cannot_be_in_past
-        if event_date_time < Time.now
+        if event_date_time.present? and event_date_time < Time.now
             errors.add(:event_date_time, "Event can not be in the past")
         end
     end
