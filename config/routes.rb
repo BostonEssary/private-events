4f08c8d8468e807fe8842de0d32b5dbd8a8f1818
Boston <bostoncessary@gmail.com>
2024-02-29 06:34:09 -0700

    Edit application_controller to allow extra devise user params

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
+  resources :users, only: [:show]
 end
