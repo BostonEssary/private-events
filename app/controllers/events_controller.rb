cc8a9f77460f923d2252b033ecf3140cbe3a4e85
Boston <bostoncessary@gmail.com>
2024-03-12 15:15:13 -0700

    add todo list

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
   
   def index
     @events = Event.all
+    @attendee = Attendee.new
   end
 
   def show
--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,5 +2,15 @@ class Attendee < ApplicationRecord
     belongs_to :user
     belongs_to :event
     
+    before_destroy :check_if_user_is_creator
     validates_uniqueness_of :user_id, :scope => :event_id
+
+    private
+
+    def check_if_user_is_creator
+        if user_id == Event.find(event_id).creator.id
+            errors.add(:base, "You can not leave an event you created, try deleting the event instead")
+            throw :abort
+        end
+    end
 end
